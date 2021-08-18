807f1193472fbd8f80d2eecaefff3dc195052eb8
Nicholas Winters <nicholas.d.winters@gmail.com>
2021-08-18 17:09:56 -0500

    updated nav

--- src/Navigation.js
+++ src/Navigation.js
@@ -5,18 +5,18 @@ import './Navigation.css';
 
 const Navigation = () => {
     return (
-        <div className='col-lg-3 mt-2'>
+        <div className='col-lg-3 mt-2 mb-3'>
             <Nav vertical pills className='nav nav-pills'>
-                <NavItem className=''>
+                <NavItem className='mb-lg-3'>
                     <NavLink exact to="/" className='Navigation-link nav-link'>About Me</NavLink>
                 </NavItem>
-                <NavItem className=''>
+                <NavItem className='mb-lg-3'>
                     <NavLink exact to="/resume" className='Navigation-link nav-link'>My Resume</NavLink>
                 </NavItem>
-                <NavItem className=''>
+                <NavItem className='mb-lg-3'>
                     <NavLink exact to="/projects" className='Navigation-link nav-link'>My Projects</NavLink>
                 </NavItem>
-                <NavItem className=''>
+                <NavItem className='mb-3'>
                     <NavLink exact to="/contact" className='Navigation-link nav-link'>Contact</NavLink>
                 </NavItem>
             </Nav>
