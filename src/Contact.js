27e234ea5854dcd8cc50ad8f09a89bdcbc8df849
Nicholas Winters <nicholas.d.winters@gmail.com>
2023-07-04 11:13:07 -0500

    remove uneeded state management

--- src/Contact.js
+++ src/Contact.js
@@ -1,56 +1,8 @@
-import React, { useEffect, useState } from 'react';
 import './App.css';
-import { send } from 'emailjs-com';
 import linkedin from './images/linkedin.png';
 import github from './images/github.png';
 
-// service_37egcdq
-
 const Contact = () => {
-    const [toSend, setToSend] = useState({
-        from_name: '',
-        message: '',
-        reply_to: '',
-    });
-
-    const [isShown, setIsShown] = useState('alert alert-primary alert-dismissible fade show d-none');
-    const [error, setError] = useState('');
-
-    const onSubmit = async (e) => {
-        e.preventDefault();
-        if (toSend.from_name.length === 0 || toSend.message.length === 0 || toSend.reply_to.length === 0) {
-            setError('Please include name, email, and a message.')
-
-        } else {
-            try {
-                await send('service_37egcdq',
-                    'template_tndlbyf',
-                    toSend,
-                    'user_xb1j08U4pypEFi4bBYciI');
-                console.log('success!');
-                setToSend({
-                    from_name: '',
-                    message: '',
-                    reply_to: '',
-                });
-                setIsShown('alert alert-primary alert-dismissible fade show d-block');
-                setError('');
-            } catch (err) {
-                console.log('failed', err);
-            }
-        }
-
-
-    };
-
-    const handleChange = (e) => {
-        setToSend({ ...toSend, [e.target.name]: e.target.value });
-    };
-
-    useEffect(() => {
-
-    }, [isShown, error]);
-
 
     return (
         <div className='col-lg-8 ms-lg-4'>
